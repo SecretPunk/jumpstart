f66735efd51a82fe72494819fdd898e0f6dcb14b
Secret Punk <amanjit.gill1@gmail.com>
2019-06-05 19:12:51 +1000

    Complete animation on navbar.

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,4 +10,35 @@ $(document).ready(function() {
     pauseOnFocus: false,
   });
 
+  var oldScrollTop;
+
+  $(window).scroll(function() {
+
+    var newScrollTop = $(this).scrollTop();
+
+    /* if scrollDistance is +ve: user is scrolling UP
+       if scrollDistance is -ve: user is scrolling DOWN */
+    var scrollDistance = oldScrollTop - newScrollTop;
+
+    var navHeight = $('.navbar').css('height').replace('px', '');
+
+    /* if screen top is now below the navbar's bottom, AND the user is scrolling DOWN,
+       then hide the navbar. */
+    if (newScrollTop > navHeight && scrollDistance < 0) {
+      $('.navbar').stop().animate({
+        top: -navHeight
+      }, 150);
+    }
+    /* if screen top is above the navbar's bottom, OR the user is scrolling UP,
+       then show the navbar. */
+    else {
+      $('.navbar').stop().animate({
+        top: 0
+      }, 150);
+    }
+
+    oldScrollTop = newScrollTop;
+
+  });
+
 });
